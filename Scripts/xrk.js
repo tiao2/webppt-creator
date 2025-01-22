173f78ead6ff3f6a05e29805070116cadc3ed59f
Tiaotiao <65841827@qq.com>
2025-01-22 10:30:07 +0800

    Update xrk.js

--- Scripts/xrk.js
+++ Scripts/xrk.js
@@ -1,1 +1,11 @@
-
+document.getElementById("input").addEventListener('keyup', function(event){
+           var a=document.getElementById('input').innerText.split('\n').length;
+           document.getElementById("line").innerText=((Array.from({length: a-1},(_,index)=> index+1)).map(text => text+'\n')).join('');
+   });
+   var module1 = document.getElementById('input'),module2 = document.getElementById('line');
+              module1.addEventListener('scroll', function() {
+                module2.scrollTop = module1.scrollTop;
+                 });
+             module2.addEventListener('scroll', function() {
+                module1.scrollTop = module2.scrollTop;
+                 });
